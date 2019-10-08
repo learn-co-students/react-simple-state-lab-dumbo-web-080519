69d2e4cfc304d247ac316cd212135959004f7556
Josh Nho <joshik@Joshs-MacBook-Pro.local>
2019-10-08 14:42:06 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Cell from './cell.js';
 
 export default class Matrix extends Component {
+
+  
   
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    vals.map(val => <Cell value={val} />) // replace me and render a cell component instead!
   )
   
   genMatrix = () => (
@@ -14,8 +17,19 @@ export default class Matrix extends Component {
     return (
       <div id="matrix">
         {this.genMatrix()}
+
       </div>
     )
   }
   
+}
+
+const newArray = []
+const colors = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+const times = 10
+for(let i=0; i < times; i++){
+  newArray.push(colors)
+}
+Matrix.defaultProps = {
+  values: newArray
 }
